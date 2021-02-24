74132481103bfcddd0e5f6adaf3b389fcc959eb3
Mia <61638568+itsmia6407@users.noreply.github.com>
2021-02-24 16:09:29 -0500

    Add project files.

---
+++ leave-management/Data/Migrations/20210224210100_UpdateLeaveRequestNumberOfDsaysToDouble.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Data.Migrations
+{
+    public partial class UpdateLeaveRequestNumberOfDsaysToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "NumberOfDays",
+                table: "LeaveAllocations",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NumberOfDays",
+                table: "LeaveAllocations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
